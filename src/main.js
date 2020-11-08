c64be0fafb105402d5b274bcb6e0f05ac829bde6
risosoky <risosoky@gmail.com>
2020-11-08 22:39:12 +0100

    pridane grafy

--- src/main.js
+++ src/main.js
@@ -3,25 +3,15 @@ import App from './App.vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueFriendlyIframe from 'vue-friendly-iframe';
-import vueSmoothScroll from 'vue2-smooth-scroll'
-import VueResizeText from 'vue-resize-text';
-import Donut from 'vue-css-donut-chart';
-import 'vue-css-donut-chart/dist/vcdonut.css';
+
  
-Vue.use(Donut);
 
-
-
-Vue.use(vueSmoothScroll)
-Vue.use(VueResizeText)
 
 import './styles/custom.scss';
 import router from './router';
 import store from "./store";
 
 
-Vue.use(VueFriendlyIframe);
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons);
 
